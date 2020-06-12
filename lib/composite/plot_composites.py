d9fb74ec35526b5b97a6b7bb63ec34b964918e78
Benjamin Held <me@bheld.eu>
2020-06-12 20:17:51 +0200

    adjusted plot calls for rain composites

--- lib/composite/plot_composites.py
+++ lib/composite/plot_composites.py
@@ -5,6 +5,7 @@ import comp_lib, rain_lib, thunderstorm_lib
 import datetime
 
 filepath = "../../files/"
+previous_data = None
 for filename in os.listdir(filepath):
   if filename.startswith("wrfout"):
     wrf_data = Nio.open_file(filepath + filename + ".nc")  # Must add ".nc" suffix for Nio.open_file
@@ -13,10 +14,16 @@ for filename in os.listdir(filepath):
     # plot composite with temperature, pressure and wind                              
     comp_lib.print_comp_for_timestamp(wrf_data, timestamp, filepath)
 
-    # plot rainsum and pressure
-    rain_lib.print_total_rainsum_for_timestamp(wrf_data, timestamp, filepath)
-
     # plot cape and pressure
     thunderstorm_lib.print_cape_for_timestamp(wrf_data, timestamp, filepath)
+
+    if (previous_data is not None):
+      # plot total rainsum and pressure
+      rain_lib.print_total_rainsum_for_timestamp(wrf_data, timestamp, filepath)
+
+      # plot 3h rainsum and pressure
+      rain_lib.get_3h_rainsum(previous_data, wrf_data, timestamp, filepath)
+      
+    previous_data = wrf_data
 
 Ngl.end()
