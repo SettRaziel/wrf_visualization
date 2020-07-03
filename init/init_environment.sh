0934fbc54cd27a06f3f4f6aa6b6dcae1ab452702
Benjamin Held <me@bheld.eu>
2020-07-03 20:21:20 +0200

    change source to conda.sh

--- init/init_environment.sh
+++ init/init_environment.sh
@@ -2,7 +2,7 @@
 # @Author: Benjamin Held
 # @Date:   2020-04-26 11:29:03
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-07-03 19:44:57
+# @Last Modified time: 2020-07-03 20:20:43
 
 # define terminal colors
 . ./terminal_color.sh
@@ -10,7 +10,7 @@
 # installs the required linux packages
 sh ./install_packages.sh
 # reload .bashrc to call conda
-. ~/.bashrc
+. /opt/miniconda3/etc/profile.d/conda.sh
 
 # installs and configs the required python packages and environments
 printf "%b\\nInstalling python dependencies: %b\\n" "${YELLOW}" "${NC}"
