546670e479e05ef318d5afea116aa89f5198aa02
Benjamin Held <me@bheld.eu>
2021-06-27 18:14:14 +0200

    reduce search time for conda packages
    * add versions to most packages, prevent requirement collision
    * update required packages for deployment
    * remove deprecated file headers

--- init/init_environment.sh
+++ init/init_environment.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# @Author: Benjamin Held
-# @Date:   2020-04-26 11:29:03
-# @Last Modified by:   Benjamin Held
-# @Last Modified time: 2020-07-03 20:20:43
 
 # define terminal colors
 . ./terminal_color.sh
@@ -14,7 +10,6 @@ sh ./install_packages.sh
 
 # installs and configs the required python packages and environments
 printf "%b\\nInstalling python dependencies: %b\\n" "${YELLOW}" "${NC}"
-conda create -n wrf_env -c conda-forge -c ncar pynio geocat-comp pyngl matplotlib cartopy jupyter netcdf4 wrf-python
-
+conda create -n wrf_env -c conda-forge -c ncar pynio=1.5.5 geocat-comp pyngl=1.6.1 matplotlib=3.4.2 cartopy jupyter=1.0.0 netcdf4 wrf-python=1.3.2 --override-channels --strict-channel-priority
 # adding high res coastlines
 sh ./configure_coastlines.sh
