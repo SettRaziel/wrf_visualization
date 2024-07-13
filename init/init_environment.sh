89a7fe4a1444fa924673d6f341e469dc2165a985
Benjamin Held <me@bheld.eu>
2024-07-13 14:44:09 +0200

    add ssl check for conda environment creation

--- init/init_environment.sh
+++ init/init_environment.sh
@@ -10,6 +10,8 @@ sh ./install_packages.sh
 
 # installs and configs the required python packages and environments
 printf "%b\\nInstalling python dependencies: %b\\n" "${YELLOW}" "${NC}"
+export CRYPTOGRAPHY_OPENSSL_NO_LEGACY=1
+conda install cryptography
 conda create -n wrf_env -c conda-forge -c ncar pynio=1.5.5 geocat-comp pyngl=1.6.1 matplotlib=3.4.2 cartopy jupyter=1.0.0 netcdf4 wrf-python=1.3.2 --override-channels --strict-channel-priority
 # adding high res coastlines
 sh ./configure_coastlines.sh
